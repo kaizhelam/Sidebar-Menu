1b22f6398aa09e3bb9b96d43b8da028d938fdf0b
kaizhelam <lamkaizhe2001@gmail.com>
2023-04-05 19:51:20 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const showMenu = (toggleId, navbarId, bodyId)=>{
+    const toggle = document.getElementById(toggleId),
+    navbar = document.getElementById(navbarId),
+    bodypadding = document.getElementById(bodyId)
+  
+    if(toggle && navbar){
+      toggle.addEventListener('click', ()=>{
+        navbar.classList.toggle('expander')
+  
+        bodypadding.classList.toggle('body-pd')
+      })
+    }
+  }
+  showMenu('nav-toggle','navbar','body-pd')
+  const linkColor = document.querySelectorAll('.nav__link')
+  function colorLink(){
+    linkColor.forEach(l=> l.classList.remove('active'))
+    this.classList.add('active')
+  }
+  linkColor.forEach(l=> l.addEventListener('click', colorLink))
+
+  const linkCollapse = document.getElementsByClassName('collapse__link')
+  var i
+  
+  for(i=0;i<linkCollapse.length;i++){
+    linkCollapse[i].addEventListener('click', function(){
+      const collapseMenu = this.nextElementSibling
+      collapseMenu.classList.toggle('showCollapse')
+  
+      const rotate = collapseMenu.previousElementSibling
+      rotate.classList.toggle('rotate')
+    })
+  }
+  
+  
